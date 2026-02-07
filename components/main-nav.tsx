f40801aef43462ea9945fc758dd79ff4cb57936e
meeeee05 <stmi.eeeee05@gmail.com>
2026-02-07 22:18:56 +0900

    ダークモード追加

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -17,7 +17,7 @@ export function MainNav() {
   return (
     <div className="flex items-center gap-6">
       <CustomLink href="/" className="text-7xl font-extrabold tracking-tight">
-        <span className="text-slate-900">Kaji</span>
+        <span className="text-slate-900 dark:text-white">Kaji</span>
         <span className="text-blue-600">Share</span>
       </CustomLink>
       <NavigationMenu>
--- components/user-button.tsx
+++ components/user-button.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "./ui/button";
-import { Bell, Settings } from "lucide-react";
+import { Bell, Settings, Sun } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -22,7 +22,23 @@ export default function UserButton() {
     <div className="flex gap-0 items-center">
       <button
         type="button"
-        className="p-1 rounded-full hover:bg-accent focus:outline-none focus:ring-2 focus:ring-blue-400 mr-2"
+        className="p-1 rounded-full hover:bg-accent focus:outline-none focus:ring-2 focus:ring-yellow-400 mr-1"
+        onClick={() => {
+          if (document.documentElement.classList.contains("dark")) {
+            document.documentElement.classList.remove("dark");
+          } else {
+            document.documentElement.classList.add("dark");
+          }
+        }}
+      >
+        <Sun
+          className="w-6 h-6 text-yellow-500"
+          aria-label="ダークモード切替"
+        />
+      </button>
+      <button
+        type="button"
+        className="p-1 rounded-full hover:bg-accent focus:outline-none focus:ring-2 focus:ring-blue-400 mr-1"
       >
         <Bell className="w-6 h-6 text-slate-500" aria-label="通知" />
       </button>
